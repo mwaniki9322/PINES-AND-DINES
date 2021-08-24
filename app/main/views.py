89d3e0cd86e992306fbad9e819dc7b94f80fe334
Dennis Mwaniki <damos5776@gmail.com>
2021-08-24 14:35:19 +0300

    update user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,abort
+from flask import render_template,abort,redirect,url_for
 from . import main
 from flask_login import login_required
 from ..models import User
+from .forms import UpdateProfile
+from .. import db
 
 
 
@@ -21,4 +23,25 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
 
