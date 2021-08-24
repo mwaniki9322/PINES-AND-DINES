deb76c064f6e14465d4dd5265ebf57ae873ff5fe
Dennis Mwaniki <damos5776@gmail.com>
2021-08-24 08:21:54 +0300

    user authentication

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mwaniki:project@localhost/project'
+
 
 class ProdConfig(Config):
     pass
