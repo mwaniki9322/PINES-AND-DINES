40d8130bad7f55143df961de1e6ee95861ddc69b
Dennis Mwaniki <damos5776@gmail.com>
2021-08-24 14:25:05 +0300

    user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,abort
 from . import main
 from flask_login import login_required
+from ..models import User
 
 
 
@@ -10,3 +11,14 @@ def index():
     view function for index page
     '''
     return render_template('index.html')
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
--- app/models.py
+++ app/models.py
@@ -15,6 +15,8 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
 
     @property
