889234a80ac31ed5ed16d94df1b90e97944b2e36
Dennis Mwaniki <damos5776@gmail.com>
2021-08-24 08:30:26 +0300

    auth blueprint

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
 
 #...
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
 
     @property
