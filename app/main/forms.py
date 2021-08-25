07458c3ca585a87a9331098b8202eac2aec70f67
Dennis Mwaniki <damos5776@gmail.com>
2021-08-25 11:51:40 +0300

    wallet table

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,18 @@
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
-from wtforms.validators import Required,Email,EqualTo
-from ..models import User
-from wtforms import ValidationError
 from flask_wtf import FlaskForm
-
-
-
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+    bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
+    submit = SubmitField('Save')
+
+class ReviewForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Clubs','Clubs'),('Hotel','Hotel'),('Restaurant','Restaurant')],validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[Required()])
+    submit = SubmitField('Review')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[Required()])
+    submit = SubmitField('Comment')
--- app/models.py
+++ app/models.py
@@ -1,45 +1,43 @@
-from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
-from . import login_manager
+from . import db,login_manager
 from datetime import datetime
+from flask_login import UserMixin,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-#...
-
-class User(db.Model,UserMixin):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    pass_secure = db.Column(db.String(255))
-    reviews = db.relationship('review', backref='user', lazy='dynamic')
+    reviews = db.relationship('Review', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
     downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
     
 
     @property
-    def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
+    @set_password.setter
     def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+        self.secure_password = generate_password_hash(password)
 
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
 
-    def verify_password(self,password):
-       return check_password_hash(self.pass_secure,password)
-
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
     def __repr__(self):
         return f'User {self.username}'
-
 
 class Review(db.Model):
     __tablename__ = 'reviews'
@@ -66,15 +64,15 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.Text(),nullable = False)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
-    reviews_id = db.Column(db.Integer,db.ForeignKey('reviews.id'),nullable = False)
+    review_id = db.Column(db.Integer,db.ForeignKey('reviews.id'),nullable = False)
 
     def save_c(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_comments(cls,reviews_id):
-        comments = Comment.query.filter_by(reviews_id=reviews_id).all()
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
 
         return comments
 
@@ -87,7 +85,7 @@ class Upvote(db.Model):
 
     id = db.Column(db.Integer,primary_key=True)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('reviews.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     
 
     def save(self):
@@ -96,19 +94,19 @@ class Upvote(db.Model):
 
     @classmethod
     def get_upvotes(cls,id):
-        upvote = Upvote.query.filter_by(reviews_id=id).all()
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
         return upvote
 
 
     def __repr__(self):
-        return f'{self.user_id}:{self.reviews_id}'
+        return f'{self.user_id}:{self.pitch_id}'
         
 class Downvote(db.Model):
     __tablename__ = 'downvotes'
 
     id = db.Column(db.Integer,primary_key=True)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    reviews_id = db.Column(db.Integer,db.ForeignKey('review.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     
 
     def save(self):
@@ -116,12 +114,11 @@ class Downvote(db.Model):
         db.session.commit()
     @classmethod
     def get_downvotes(cls,id):
-        downvote = Downvote.query.filter_by(review_id=id).all()
+        downvote = Downvote.query.filter_by(pitch_id=id).all()
         return downvote
 
     def __repr__(self):
-        return f'{self.user_id}:{self.review_id}'
-
+        return f'{self.user_id}:{self.pitch_id}'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
